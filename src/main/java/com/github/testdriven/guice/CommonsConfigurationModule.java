0a04e8246bf2823dbf086ee28a19e9703e8ea0b1
Maciej Hryniszak <maciej.hryniszak@lumesse.com>
2014-11-13 10:52:27 +0100

    Initial import

---
+++ src/main/java/com/github/testdriven/guice/CommonsConfigurationModule.java
@@ -0,0 +1,19 @@
+package com.github.testdriven.guice;
+
+import org.apache.commons.configuration.Configuration;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.matcher.Matchers;
+
+public class CommonsConfigurationModule extends AbstractModule {
+	private final Configuration configuration;
+
+	public CommonsConfigurationModule(Configuration configuration) {
+		this.configuration = configuration;
+	}
+
+	@Override
+	protected void configure() {
+		bindListener(Matchers.any(), new ValueListener(configuration));
+	}
+}
